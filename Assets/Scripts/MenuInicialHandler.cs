50a7d6dd79d8175e73c6c29f380f55f769ce96b5
Bruno Bennotti B Moura <brunobennotti@gmail.com>
2021-10-09 15:49:30 -0300

    feat: troca de scene feat(MenuInicial): onclick iniciar feat(MenuInicial): onclick opções

--- Assets/Scripts/MenuInicialHandler.cs
+++ Assets/Scripts/MenuInicialHandler.cs
@@ -1,12 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuInicialHandler : MonoBehaviour
 {
     public void Awake()
     {
-        var a = transform.Find("Iniciar");
+        var btnIniciar = GameObject.Find("/Canvas/Iniciar").GetComponent<Button>();
+        var btnOpcoes = GameObject.Find("/Canvas/Opcoes").GetComponent<Button>();
+
+        btnIniciar.onClick.AddListener(delegate { IrParaGameScene(); });
+        btnOpcoes.onClick.AddListener(delegate { IrParaOpcoes(); });
+
+        Debug.Log("ok");
+    }
+
+    void IrParaGameScene()
+    {
+        Loader.Load(Loader.Scene.GameScene);
+        Debug.Log("ok");
+    }
+
+    void IrParaOpcoes()
+    {
+        Loader.Load(Loader.Scene.OpcoesScene);
         Debug.Log("ok");
     }
 }
